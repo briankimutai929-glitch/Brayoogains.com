019959ee5880eb959cdc4215a582bc51eeefefab
briankimutai929-glitch <briankimutai929@gmail.com>
2026-07-10 22:07:50 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,4 +1,4 @@
-const db = require("./config/database");const express = require("express");
+app.use("/api/auth", authRoutes);const db = require("./config/database");const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
